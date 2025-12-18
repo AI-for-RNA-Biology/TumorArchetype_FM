d3c8ca1640f3d477281a90a639abe3641028e176
lisafournier <44362563+lisafournier@users.noreply.github.com>
2025-12-18 15:19:07 +0100

    Create access_token.py for HF token handling
    Add a new file for Hugging Face access token management.

---
+++ digitalhistopathology/access_token.py
@@ -0,0 +1,9 @@
+"""
+Your HF access tokens should remain private, never share them. You can also set your HuggingFace 
+read-only token as an environment variable: export HF_TOKEN=<MY_READ_ONLY_TOKEN>. If set, this value 
+will overwrite the token stored on the machine (in either $HF_TOKEN_PATH or "$HF_HOME/token" if the 
+former is not set).
+
+"""
+
+READ_ONLY_HF_TOKEN = "hf_XXXXXXXX"
